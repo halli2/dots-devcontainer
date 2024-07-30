1ae7b1bf2683de26587cc26b079da1576ecaabfa
Halvor Kvamme <flkz@proton.me>
2024-07-30 16:21:39 +0200

    Fix zellij install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo dnf -y copr enable varlad/zellij 
+sudo dnf -y install 'dnf-command(copr)'
+sudo dnf -y copr enable varlad/zellij
 sudo dnf -y install helix fish zellij
 mv config ~/.config
